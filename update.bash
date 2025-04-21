d91360d592f0d36344ae735c9bee17f9fb83c159
amal-J-k <143809111+amal-J-k@users.noreply.github.com>
2025-04-22 00:24:57 +0530

    Add files via upload

---
+++ update.bash
@@ -0,0 +1,10 @@
+sudo apt-get update -y
+sudo apt update -y
+sudo apt upgrade -y
+sudo snap refresh
+sudo apt dist-upgrade -y
+sudo apt full-upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean -y
+sudo do-release-upgrade 
+sudo snap refresh
