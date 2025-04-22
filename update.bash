1a2f51b83621819b2a46a4852df02ae7b8275dd1
amal-J-k <143809111+amal-J-k@users.noreply.github.com>
2025-04-22 23:24:53 +0530

    added commenting

--- update.bash
+++ update.bash
@@ -1,7 +1,31 @@
+#!/bin/bash
+
+# Update package index
+echo "Updating package index..."
 sudo apt update -y
+
+# Upgrade all installed packages to the latest version
+echo "Upgrading all packages..."
 sudo apt full-upgrade -y
+
+# Upgrade to the latest version of Ubuntu, if available
+echo "Upgrading to the latest version of Ubuntu..."
 sudo apt dist-upgrade -y
+
+# Remove unnecessary packages
+echo "Removing unnecessary packages..."
 sudo apt autoremove -y
+
+# Remove unnecessary package files
+echo "Removing unnecessary package files..."
 sudo apt autoclean -y
-sudo do-release-upgrade 
+
+# Upgrade to the next Ubuntu release, if available
+echo "Upgrading to the next Ubuntu release..."
+sudo do-release-upgrade
+
+# Refresh snap packages
+echo "Refreshing snap packages..."
 sudo snap refresh
+
+echo "Update complete!"
